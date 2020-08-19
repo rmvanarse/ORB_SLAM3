eab3456c4690507c966149ba1fecdbbbc10c857e
Rishikesh Vanarse <rishikesh.vanarse@gmail.com>
2020-08-19 23:57:15 +0530

    Solved stereo frequent resets

--- Examples/ROS/ORB_SLAM3/src/pose_to_path.py
+++ Examples/ROS/ORB_SLAM3/src/pose_to_path.py
@@ -8,10 +8,29 @@ from geometry_msgs.msg import PoseStamped
 
 path = Path()
 count =0
+path_len = 0
+x_prev, y_prev, z_prev = 0,0,0
 
 def pose_cb(data):
     global path
     global count
+    global path_len, x_prev, y_prev, z_prev
+
+    x = data.pose.position.x
+    y = data.pose.position.y
+    z = data.pose.position.z
+
+    dist = ((x-x_prev)**2 + (y-y_prev)**2 + (z-z_prev)**2)**0.5
+    
+    #Temporary value of 0.5 used for eliminating jumps. CHANGE THIS LATER
+    if dist < 0.5:
+        path_len = path_len + dist
+    else:
+        path_len = 0
+    
+    x_prev, y_prev, z_prev = x,y,z
+    #print("Path Length = "+str(path_len))
+
     path.header = data.header
 
     path.poses.append(data)
@@ -23,6 +42,7 @@ def pose_cb(data):
         odom.header = data.header
         odom.pose.pose = data.pose
         odom_pub.publish(odom)
+        print("Path Length = "+str(path_len))
 
 
 if __name__ == '__main__':
